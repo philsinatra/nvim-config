dd160d7bfb160025b774358e8ea2dc7214de0234
Phil Sinatra <code@philsinatra.com>
2025-08-06 11:06:46 -0400

    Change tab size to 4

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,8 @@
 -- General settings
 vim.opt.number = true          -- Show line numbers
 vim.opt.relativenumber = true  -- Relative line numbers
-vim.opt.tabstop = 2            -- 2 spaces for tabs
-vim.opt.shiftwidth = 2         -- 2 spaces for indent
+vim.opt.tabstop = 4            -- 2 spaces for tabs
+vim.opt.shiftwidth = 4         -- 2 spaces for indent
 vim.opt.expandtab = true       -- Use spaces instead of tabs
 vim.opt.smartindent = true     -- Auto-indent new lines
 vim.opt.wrap = true            -- Disable line wrapping
